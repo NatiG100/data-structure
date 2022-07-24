7ff382909da3adb7249dce32a003b361d701a43e
NatiG100 <kalebok39@gmail.com>
2022-07-24 03:42:36 -0700

    inserting to a list

---
+++ LinkedList/SinglyLinkedList.cpp
@@ -0,0 +1,70 @@
+#include<iostream>
+#include<stdlib.h>
+
+struct ListNode{
+	int data;
+	struct ListNode *next;
+};
+struct ListNode *HEAD;
+
+int ListLength(struct ListNode *head);
+void printList(struct ListNode *head);
+void InsertInLinkedList(struct ListNode **head,int data, int position);
+
+using namespace std;
+int main(){
+	InsertInLinkedList(&HEAD,1,1);
+	InsertInLinkedList(&HEAD,2,2);
+	InsertInLinkedList(&HEAD,3,3);
+	InsertInLinkedList(&HEAD,4,2);
+	printList(HEAD);
+	return 0;
+}
+
+
+int ListLength(struct ListNode *head){
+	struct ListNode *current = head;
+	int count = 0;
+	while(current!=NULL){
+		count++;
+		current = current->next;
+	}
+	return count;
+}
+void printList(struct ListNode *head){
+	struct ListNode *current = head;
+	while(current!=NULL){
+		cout<<current->data<<", ";
+		current= current->next;
+	}
+}
+void InsertInLinkedList(struct ListNode **head,int data, int position){
+	int k=1;
+	struct ListNode *p,*q,*newNode;
+	newNode = (ListNode *) malloc(sizeof(struct ListNode));
+	if(!newNode){
+		printf("Memory Error");
+		return;
+	}
+	newNode->data = data;
+	p = *head;
+	if(position==1){
+		newNode->next = p;
+		*head = newNode;
+	}
+	else{
+		while((p!=NULL)&&(k<position)){
+			k++;
+			q=p;
+			p=p->next;
+		}
+		if(p==NULL){
+			q->next = newNode;
+			newNode->next = NULL;
+		}
+		else{
+			q->next = newNode;
+			newNode->next = p;
+		}
+	}
+}
